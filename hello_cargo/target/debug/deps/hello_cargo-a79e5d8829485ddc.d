97200c2093b9d2767101ab418204615f78145b77
HsiaoCz <984274788@qq.com>
2023-04-29 04:47:39 +0800

    rust

---
+++ hello_cargo/target/debug/deps/hello_cargo-a79e5d8829485ddc.d
@@ -0,0 +1,5 @@
+/home/hsiaocz/rust-code/hello_cargo/target/debug/deps/hello_cargo-a79e5d8829485ddc: src/main.rs
+
+/home/hsiaocz/rust-code/hello_cargo/target/debug/deps/hello_cargo-a79e5d8829485ddc.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/hsiaocz/rust-code/hello_cargo/target/debug/hello_cargo: /home/hsiaocz/rust-code/hello_cargo/src/main.rs
