43458e8b9cf5c6a9139c84b68a5e3de824cfdc53
HsiaoCz <984274788@qq.com>
2023-05-06 12:15:04 +0800

    rust

--- variables/string/main.rs
+++ variables/string/main.rs
@@ -1,5 +1,13 @@
 fn main() {
     let mut s = String::from("Hello");
     s.push_str(",World");
-    println!("{}", s)
+    println!("{}", s);
+
+    let s1 = String::from("hello");
+    // let s2 = s1;
+    // 这里会报错，s1已经被释放了
+    println!("{}", s1);
+    // 使用clone可以
+    let s2 = s1.clone();
+    println!("{},{}", s1, s2);
 }
