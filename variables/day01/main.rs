f0afc6217e3e109ab039324afb3107d2f9be6032
HsiaoCz <984274788@qq.com>
2024-04-10 15:08:22 +0800

    cargo

---
+++ variables/day01/main.rs
@@ -0,0 +1,13 @@
+fn main(){
+    let mut s=String::from("Hello");
+    s.push_str(",World");
+    println!("{}",s);
+
+    let s1=String::from("hello");
+    // let s2=s1
+    // 这里会报错 s1已经释放了
+    println!("{}",s1);
+    // 使用clone 可以
+    let s2=s1.clone();
+    println!("{},{}",s1,s2)
+}
